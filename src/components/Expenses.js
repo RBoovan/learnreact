c88a8400ec45d6854fb883b9f808046b01125d2d
Lokesh <mail.lokesh.balaji@gmail.com>
2022-01-08 14:13:22 +0530

    'learnreact'

---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import './Expenses.css'
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            <h2>Let's get started!</h2>
+            <ExpenseItem
+                title={props.data[0].title}
+                amount={props.data[0].amount}
+                date={props.data[0].date}
+            />
+            <ExpenseItem
+                title={props.data[1].title}
+                amount={props.data[1].amount}
+                date={props.data[1].date}
+            />
+            <ExpenseItem
+                title={props.data[2].title}
+                amount={props.data[2].amount}
+                date={props.data[2].date}
+            />
+            <ExpenseItem
+                title={props.data[3].title}
+                amount={props.data[3].amount}
+                date={props.data[3].date}
+            />
+        </Card>
+    );
+    
+}
+export default Expenses
